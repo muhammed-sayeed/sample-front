3436588a6959bc45eff5f5ff142aa483994451d0
muhammed-sayeed <sayeedmon25@gmail.com>
2023-09-19 13:58:49 +0530

    url changed

--- src/app/featureModule/services/user.service.ts
+++ src/app/featureModule/services/user.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from "@angular/common/http";
 import { loginInterface } from "src/app/coreModule/interfaces/login.interface";
 import { tokenResponce } from "src/app/coreModule/interfaces/loginRespons.interface";
 import {  productInterface } from "src/app/coreModule/interfaces/product.interface";
+import { environment } from "src/environments/environment.development";
 
 @Injectable({
     providedIn:'root'
@@ -12,16 +13,16 @@ export class userServise{
     constructor(private http:HttpClient){}
     
     userRegister(data:any):Observable<tokenResponce>{
-      return this.http.post<tokenResponce>('http://localhost:3000/register',data)
+      return this.http.post<tokenResponce>(environment.baseUrl+'/register',data)
     }
     userLogin(data:loginInterface):Observable<tokenResponce>{
-      return this.http.post<tokenResponce>('http://localhost:3000/login',data)
+      return this.http.post<tokenResponce>(environment.baseUrl+'/login',data)
     }
     setToken(accessToken:string,refreshToken:string){
         localStorage.setItem('accessToken',accessToken)
         localStorage.setItem('refreshToken',refreshToken)
     }
     fetchProduct(page:number,pageSize:number):Observable<productInterface[]>{
-      return this.http.get<productInterface[]>(`http://localhost:3000/fetchproducts?page=${page}&pageSize=${pageSize}`)
+      return this.http.get<productInterface[]>(environment.baseUrl+`/fetchproducts?page=${page}&pageSize=${pageSize}`)
     }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    baseUrl:'https://onzooserver-com.onrender.com'
+};
